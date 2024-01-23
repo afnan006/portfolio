04fc3db78b6cd2639621b075dc095cc4ed4aa922
afnan006 <afnan006cs@gmail.com>
2024-01-24 00:45:17 +0530

    cards animated

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('section');
     const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
 
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5, // Adjust the threshold as needed
+    };
+
+    const sectionObserver = new IntersectionObserver(handleIntersection, observerOptions);
+
+    sections.forEach((section) => {
+        sectionObserver.observe(section);
+    });
+
+    function handleIntersection(entries) {
+        entries.forEach((entry) => {
+            const sectionId = entry.target.id;
+            const correspondingNavLink = document.querySelector(`[href="#${sectionId}"]`);
+
+            if (entry.isIntersecting) {
+                navLinks.forEach(function (navLink) {
+                    navLink.classList.remove('active');
+                });
+
+                correspondingNavLink.classList.add('active');
+            }
+        });
+    }
+
+    // Additional code for handling link clicks
     navLinks.forEach(function (link) {
         link.addEventListener('click', function () {
             navLinks.forEach(function (navLink) {
@@ -12,3 +40,22 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+document.addEventListener('DOMContentLoaded', function () {
+    const cards = document.querySelectorAll('.card-container');
+
+    cards.forEach(function (card) {
+        card.addEventListener('mouseenter', function () {
+            if (card.classList.contains('card-container-2')) {
+                card.querySelector('.card').classList.add('jumping-down');
+            } else {
+                card.querySelector('.card').classList.add('jumping-up');
+            }
+        });
+
+        card.addEventListener('mouseleave', function () {
+            card.querySelector('.card').classList.remove('jumping-up', 'jumping-down');
+        });
+    });
+});
+
+
